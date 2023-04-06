d319b961112e7111dd9d378d0e3d57aa5ec5ca1c
Akshatha2022 <115680614+Akshatha2022@users.noreply.github.com>
2023-04-06 17:31:15 -0500

    modyfing logic

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -26,7 +26,7 @@ const jateDb = await openDB('jate', 1);
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({ text: content });
+  const request = store.put({ id: 1, value: content });
 
    // Get confirmation of the request.
   const result = await request;
